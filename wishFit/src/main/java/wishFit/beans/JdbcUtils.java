8e917b5fdb19b661c2b7a05de320fc4fb3ec8d16
beetrbgus <devbeetrb0214@gmail.com>
2021-11-18 14:03:40 +0900

    이미 존재해서 jdbcutil 지움. 수저하고 하세요. 메인 페이지 주석 추가.

--- wishFit/src/main/java/wishFit/beans/JdbcUtils.java
+++ wishFit/src/main/java/wishFit/beans/JdbcUtils.java
@@ -8,29 +8,22 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 public class JdbcUtils {
-		public static Connection connect(String username, String password) throws Exception {
-			Class.forName("oracle.jdbc.OracleDriver");
-			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", username, password);
+	
+	//  DataSource = DBCP Factory. 연결 객체를 생성하고. 
+	private static DataSource ds;
 
-			return con;
+	static {
+		try {
+			Context ctx = new InitialContext();
 
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/oracle");
+		} catch (Exception e) {
+			System.err.println("DataSource 생성 오류");
+			e.printStackTrace();
 		}
-		
+	}
 
-		private static DataSource ds;
-
-		static {
-			try {
-				Context ctx = new InitialContext();
-				
-				ds = (DataSource) ctx.lookup("java:comp/env/jdbc/oracle");
-			} catch (Exception e) {
-				System.err.println("DataSource 생성 오류");
-				e.printStackTrace();
-			}
-
-		}
-		public static Connection connect2() throws Exception{
-			return ds.getConnection();
-		}
+	public static Connection connect() throws Exception {
+		return ds.getConnection();
+	}
 }
