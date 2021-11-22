25f7654d1ecee9dcb90a0614b89847d8bc550a5c
choihyunjoon1 <992715@naver.com>
2021-11-22 18:18:18 +0900

    구현중인것

---
+++ wishFit/src/main/java/wishFit/beans/image/ImageDao.java
@@ -0,0 +1,17 @@
+package wishFit.beans.image;
+
+public class ImageDao {
+
+	public void insert(ImageDto imageDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//Image 등록 처리
+	
+	//Image 수정 처리? ( 수정페이지에서 존재하는걸 삭제 / 존재하는걸 변경( 존재하는걸 삭제 후 새로운거 등록) / 없는 파일 등록 ) 인데 update써야하나..?
+	
+	//Image 삭제 처리
+	
+	//Image 읽기 처리
+}
