21f74a003b58318222b5652e39a6df6be1490146
choihyunjoon1 <992715@naver.com>
2021-11-29 15:59:13 +0900

    이미지 다오

---
+++ wishFit/src/main/java/wishFit/beans/image/ImageDto.java
@@ -0,0 +1,59 @@
+package wishFit.beans.image;
+
+public class ImageDto {
+	private int imageNo;
+	private int boardNo;
+	private String boardUpload;
+	private String boardSave;
+	private Long boardSize;
+	private String boardType;
+	public int getImageNo() {
+		return imageNo;
+	}
+	public void setImageNo(int imageNo) {
+		this.imageNo = imageNo;
+	}
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getBoardUpload() {
+		return boardUpload;
+	}
+	public void setBoardUpload(String boardUpload) {
+		this.boardUpload = boardUpload;
+	}
+	public String getBoardSave() {
+		return boardSave;
+	}
+	public void setBoardSave(String boardSave) {
+		this.boardSave = boardSave;
+	}
+	public Long getBoardSize() {
+		return boardSize;
+	}
+	public void setBoardSize(Long boardSize) {
+		this.boardSize = boardSize;
+	}
+	public String getBoardType() {
+		return boardType;
+	}
+	public void setBoardType(String boardType) {
+		this.boardType = boardType;
+	}
+	public ImageDto() {
+		super();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
